e89344a150e80a7c414f2381cfd9faa606b86685
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-10-15 17:25:57 +0000

    files update

--- spotify-clone/app/layout.tsx
+++ spotify-clone/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 
 import Sidebar from '@/components/Sidebar'
+import SupabaseProvider from '@/providers/SupabaseProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -20,11 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>
-        {children}
-        </Sidebar>
-        
-        </body>
+        <SupabaseProvider>
+          <Sidebar>
+            {children}
+          </Sidebar>
+        </SupabaseProvider>
+      </body>
     </html>
   )
 }
